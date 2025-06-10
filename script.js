5ac3d38664fb075b19cd3c5dd945182ad8b3e7eb
Chesantz <smunozriv@gmail.com>
2025-06-10 16:56:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
+    document.body.classList.add('no-scroll'); // bloquear scroll al abrir
 }
-function hideSidebar(){
+
+function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
+    document.body.classList.remove('no-scroll'); // permitir scroll al cerrar
 }
